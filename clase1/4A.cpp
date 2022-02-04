16c8e40aa07706162b07478a46027d07647a4825
Wilmer Rafael Bandres Hernandez <wilmer@bakkenbaeck.no>
2022-02-04 20:17:57 +0100

    initial commit

---
+++ clase1/4A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+        //Definimos una variable de tipo entero
+        int w;
+
+        cin >> w;
+
+        if(w % 2 == 0) {
+             cout << "YES" << endl;   
+        } else {
+                cout << "NO" << endl;
+        }
+
+        
+        return 0;
+}
